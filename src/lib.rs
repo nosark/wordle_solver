ee8a8f2652f2c252b25b462061963e9e75fc7c0f
kyle nosar <kylenosar@gmail.com>
2022-03-07 22:16:01 -0800

    init commit

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+pub mod wordle;
+pub mod wordle_solver;
---
+++ src/wordle.rs
@@ -0,0 +1,10 @@
+pub mod wordle {
+    #[allow(dead_code)]
+    pub struct WordleBoard {
+        board: Vec<[String; 5]>,
+        guesses: i8,
+        word: String,
+    }
+
+    impl WordleBoard {}
+}
